96473985de5a35fdff48281659d0122c27e55483
배준혁 (JuneHyuck Bae) <37135285+BaeJuneHyuck@users.noreply.github.com>
2019-04-01 22:03:20 +0900

    myChat.c

--- myChat.c
+++ myChat.c
@@ -1,19 +1,19 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <stdlib.h> // getenv, malloc, free
-#include <string.h>
+#include <string.h> //strcpy, strncpy, strcat
 
 #define MAX_USERNAME 32
-#define MAX_BUFFER 15360
+#define MAX_BUFFER 2048 
 #define MAX_MESSAGE_LENGTH 185
 #define MAX_OUTLINE 15
 
 const int output_x = 50;
 const int output_y = 18;
-const int input_x =  50;//output_x
+const int input_x =  50;
 const int input_y = 6;
 const int userlist_x = 30;
-const int userlist_y = 24;//output_y + input_y;
+const int userlist_y = 24;
 
 typedef struct message{
   char *str;
@@ -33,6 +33,8 @@ void initQueue (messageQueue* q){
   q->size = 0;
 }
 
+// add a message to message queue
+// using dynamic memory allocation
 void enqueue(messageQueue* q, char* _from, char* _str, int length){
   q->size++;
   message* temp = (message *)malloc(sizeof(message));
@@ -43,6 +45,7 @@ void enqueue(messageQueue* q, char* _from, char* _str, int length){
   strcpy(temp->from,_from);
   strncpy(temp->str,_str,length);
 
+  // if queue is empty or not
   if(q->rear == NULL) q->front = q->rear = temp;
   else{
     q->rear->link = temp;
@@ -50,6 +53,8 @@ void enqueue(messageQueue* q, char* _from, char* _str, int length){
   }
 }
 
+// delete message from queue
+// free used memory
 void dequeue(messageQueue* q){
   q->size--;
   message* temp = q->front;
@@ -60,14 +65,16 @@ void dequeue(messageQueue* q){
   free(temp);
  }
 
+// print all Messages in messagequeue
+// get message instance with temp->link
+// merge all characters into one 'totalStr' with strcat
 void printMessages(WINDOW* output, messageQueue* q){
   char *totalStr = (char *)malloc(MAX_BUFFER);
-  strcpy(totalStr,"\0");
+  strcpy(totalStr,"\0"); // init str '\0'
   message* temp = q->front;
   
   int t = 0;
 
-  // queue message -> print  
   while(temp){
     char *curStr = (char *)malloc(MAX_MESSAGE_LENGTH + MAX_USERNAME + 6);
     strcpy(curStr," "); // set string empty
@@ -79,17 +86,20 @@ void printMessages(WINDOW* output, messageQueue* q){
     strcat(curStr,"\0");
     size_t totallen = strlen(totalStr);
     size_t curlen = strlen(curStr);
+    
+    // concatenate current string(temp) to total string
     strcat(totalStr,curStr);
     totalStr[totallen]='\n';
-    
     totalStr[totallen+curlen+2]='\0';
+    
+    // select next message, free temp char*
     temp = temp->link;
     free(curStr);
   }
   
   wclear(output);
-  mvwprintw(output, 0, 0, totalStr);
-  free(totalStr);
+  mvwprintw(output, 0, 0, totalStr); // finally print total string
+  free(totalStr); // don't forget free
 }
 
 void printWindow(char* user,WINDOW* input,WINDOW* output, WINDOW* userlist){
@@ -106,16 +116,17 @@ void printWindow(char* user,WINDOW* input,WINDOW* output, WINDOW* userlist){
 }
 
 int main(){
-  char *username;
+  // mqueue has all messages from user
   messageQueue* mqueue = (messageQueue *)malloc(sizeof(messageQueue));
   
+  char *username;
+  username = getenv("USER");
+
   initQueue(mqueue);
   initscr();
   noecho();
   cbreak();
-  //curs_set(false);
-  username = getenv("USER");
-
+ 
   // make window
   WINDOW *output_border   = newwin(output_y, output_x, 0, 0);
   WINDOW *userlist_border = newwin(userlist_y, userlist_x, 0, output_x);
@@ -131,12 +142,14 @@ int main(){
   start_color();
   init_pair(1,COLOR_BLACK,COLOR_WHITE);
 
+  //make border(box)
   box(input_border,0,0);
   box(userlist_border,0,0);
   box(output_border,0,0);
   wrefresh(output_border);
   wrefresh(userlist_border);
   wrefresh(input_border);
+  
   printWindow(username, input,output,userlist);
 
   char inputline[MAX_MESSAGE_LENGTH] ="";
@@ -144,24 +157,28 @@ int main(){
   char outputline[MAX_BUFFER] = " ";
   char c;
   while(c = wgetch(input)){ 
-    if(c == 27) break;  // esc
-    else if(c == 10){ // enter
+    if(c == 27) break;  // get 'esc' -> exit
+    else if(c == 10){ 
+      // enter, make a message from inputline and enqueue
+      // if queue is full dequeue the oldest one
       if (index <= 0) continue;
       if(mqueue->size > MAX_OUTLINE) dequeue(mqueue);
       inputline[index++] = '\0';
       enqueue(mqueue,username,inputline,index);
-      printMessages(output,mqueue);
+      printMessages(output,mqueue); //update output screen
       
       //clear input line
       memset(inputline,'\0',MAX_MESSAGE_LENGTH);
       index = 0;
       wclear(input);
-    }else if(c == 127){ // backspace
+    }else if(c == 127){
+      // backspace, remove the last chracter from inputline
       if (index <= 0) continue;
       inputline[index-1] = '\0';
       index--;
       wclear(input);
     }else{
+      // else, add a chracter to inputline
       if (index >= MAX_MESSAGE_LENGTH)continue;
       inputline[index] = c;
       inputline[index+1] = '\0';
